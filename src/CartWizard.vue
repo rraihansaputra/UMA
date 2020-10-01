ea76f52e8da6104d468ceec8dec0407ee4640ce9
raihan saputra <rraihansaputra@gmail.com>
2020-10-01 21:00:34 +0700

    add todo comment

--- src/CartWizard.vue
+++ src/CartWizard.vue
@@ -574,6 +574,7 @@ export default Vue.extend({
       this.dataLoaded = true;
     },
     openSubscriptionDetails() {
+      // TODO scrollIntoView with center?
       document.getElementById("subscription").open = true;
     },
     loadParams() {
